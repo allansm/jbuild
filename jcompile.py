72560eccadc7dc09535a3a01dacfdba18c4fdf9c
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-04-24 19:36:02 -0300

    Create jcompile

---
+++ jcompile.py
@@ -0,0 +1,26 @@
+import os
+from jcore import *
+from shutil import rmtree
+
+src = getAllFiles("src")
+
+try:
+    rmtree("bin")
+except:
+    dummy = ""
+
+os.mkdir("bin")
+
+javac = []
+
+for f in src:
+    if ".java" in f:
+        javac.append(os.path.dirname(f)+"/"+"*.java")
+
+javac = list(dict.fromkeys(javac))
+
+for line in javac:
+    print(line)
+
+for line in javac:
+    os.system("javac -d bin "+line)
